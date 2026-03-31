21b867d6e02c08e8a42e0308adf61597da3181c7
WhXteBeXr <WhiteBear_LT@outlook.com>
2026-03-31 16:45:40 +0300

    Выполнено задание 6

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -14,6 +14,7 @@ int main()
           "\n3 - Строчные в прописные"
           "\n4 - Позиции чисел"
           "\n5 - Самый молодой"
+          "\n6 - Произведение чисел"
        << endl;
   cin >> userChoice;
   cout << endl;
@@ -36,14 +37,19 @@ int main()
     taskFive();
     break;
   case 6:
+    taskSix();
     break;
   case 7:
+    taskSeven();
     break;
   case 8:
+    taskEight();
     break;
   case 9:
+    taskNine();
     break;
   case 10:
+    taskTen();
     break;
 
   default:
